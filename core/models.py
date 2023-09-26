940383a55caeab000db286beef09ac3ceff3913b
NiramayThaker <thakerniramay@gmail.com>
2023-09-26 18:41:20 +0530

    Creating line chart

--- core/models.py
+++ core/models.py
@@ -6,9 +6,9 @@ class CO2(models.Model):
 	date = models.DateField()
 	average = models.FloatField()
 
-	def __str__(self):
-		return f"{self.date}"
-
 	class Meta:
 		# Will be ordered from earlier to latest
 		ordering = ('date',)
+
+	def __str__(self):
+		return f"{self.date}"
--- core/views.py
+++ core/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from core.models import CO2 
+import plotly.express as px
+
 
 # Create your views here.
+def charts(request):
+    co2 = CO2.objects.all()
+
+    fig = px.line(
+        x = [co2.date for c in co2],
+        y = [co2.average for c in co2]
+    )
+    
+    chart = fig.to_html()
+
+    context = {'chart': chart}
+    return render(request, 'core/charts.html', context)
