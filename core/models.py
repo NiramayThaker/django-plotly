cbb404225e35aedc5e005bb1f50edcc52cb13316
NiramayThaker <thakerniramay@gmail.com>
2023-09-26 18:14:08 +0530

    --

--- core/models.py
+++ core/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
 class CO2(models.Model):
-    data = models.DateField()
-    average = models.FloatField()
+	date = models.DateField()
+	average = models.FloatField()
 
-    class Meta:
-        # Will be ordered from earlier to latest 
-        ordering = ('date', )
+	def __str__(self):
+		return f"{self.date}"
+
+	class Meta:
+		# Will be ordered from earlier to latest
+		ordering = ('date',)
